7d8c39b2ada5e55b347a4b9f1f00e8e4c34a1cd6
jblici <juambif@gmail.com>
2021-05-13 13:15:19 -0300

    Creacion de modelo Brand y asociacion

---
+++ src/models/Brand.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+const DT = DataTypes;
+
+module.exports = (sequelize) => {
+    sequelize.define('brands', {
+        id: {
+            type: DT.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+        name: {
+            type: DT.STRING,
+            unique: true,
+            allowNull: false
+        },
+    },
+        {
+            timestamps: false,
+            underscored: true
+        });
+}
