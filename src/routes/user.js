59442ac2eef6a0f71f02f6e6162d702a50c2322f
macamparone <marco43@hotmail.com>
2021-05-14 19:21:13 -0300

    Created Users routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,76 @@
 const { Router } = require("express");
 const { User } = require('../models/User')
+const { getAllUsers, getUserById, createUser } = require("../controllers/user");
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('user')
+router.get('/', async (req, res) => {
+    try {
+        const users = await getAllUsers()
+        res.send(users)
+    } catch (error) {
+        res.send(error)
+    }
 })
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const userID = await getUserById(id);
+        res.send(userID)
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+router.post("/", async (req, res) => {
+    console.log(req.body);
+  try {
+    const {
+        id,
+      first_name,
+      last_name,
+      is_admin,
+      email,
+      password,
+      country,
+      city,
+      street,
+      addressnumber,
+      postcode,
+    } = req.body;
+    if (
+    !id ||
+      !first_name ||
+      !last_name ||
+      !is_admin ||
+      !email ||
+      !password ||
+      !country ||
+      !city ||
+      !street ||
+      !addressnumber ||
+      !postcode
+    )
+    res.send("usuario invalido");
+    const userCreated = await createUser(
+      id,
+      first_name,
+      last_name,
+      is_admin,
+      email,
+      password,
+      country,
+      city,
+      street,
+      addressnumber,
+      postcode
+    );
+    res.send(userCreated);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
 
 module.exports = router;
