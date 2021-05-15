377243b78ad42c8f1735a1d8785eaa84d4c5cfa3
Leonardo Rosales <leonardorosales1485@gmail.com>
2021-05-15 17:16:38 -0300

    feat/Orders GET REQUESTS

--- db.js
+++ db.js
@@ -1,13 +1,10 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
-const fs = require('fs');
-const { DataTypes } = require('sequelize');
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const { DataTypes } = require("sequelize");
 const DT = DataTypes;
-const path = require('path');
-const {
-    DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
-} = process.env;
-
+const path = require("path");
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 let sequelize;
 if (process.env.DATABASE_URL !== undefined) {
@@ -44,46 +41,65 @@ const basename = path.basename(__filename);
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/src/models'))
-    .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
-    .forEach((file) => {
-        modelDefiners.push(require(path.join(__dirname, '/src/models', file)));
-    });
+fs.readdirSync(path.join(__dirname, "/src/models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, "/src/models", file)));
+  });
 
 // Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach(model => model(sequelize));
+modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+let capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-const { Brand, Category, Image, Order, OrderProduct, Product, Review, User, OrderLine } = sequelize.models;
-
+const {
+  Brand,
+  Category,
+  Image,
+  Order,
+  Product,
+  Review,
+  User,
+  OrderLine,
+} = sequelize.models;
 
 // Aca vendrian las relaciones
 Product.hasMany(Image, { foreignKey: { allowNull: false } });
 Product.hasMany(Review, { foreignKey: { allowNull: false } });
-Product.belongsTo(Brand, { foreignKey: { allowNull: true, type: DT.INTEGER } })
-Product.belongsToMany(Category, { through: 'products_categories' });
-Category.belongsToMany(Product, { through: 'products_categories' });
-Brand.hasMany(Product, { foreignKey: { allowNull: false }})
-Product.belongsToMany(Order, { through: 'orders_products' })
-Order.belongsToMany(Product, { through: 'orders_products' })
+Product.belongsTo(Brand, { foreignKey: { allowNull: true, type: DT.INTEGER } });
+
+Product.belongsToMany(Category, { through: "products_categories" });
+Category.belongsToMany(Product, { through: "products_categories" });
+
+Brand.hasMany(Product, { foreignKey: { allowNull: false } });
+
+Product.belongsToMany(Order, { through: "orders_products" });
+Order.belongsToMany(Product, { through: "orders_products" });
+
 User.hasMany(Order, { foreignKey: { allowNull: false } });
 User.hasMany(Review, { foreignKey: { allowNull: false } });
 Review.belongsTo(User, { foreignKey: { allowNull: false } });
 
-// verifico conexion a la base de datos
-sequelize.authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
 
+// verifico conexion a la base de datos
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 module.exports = {
-    ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-    conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+  conn: sequelize, // para importart la conexión { conn } = require('./db.js');
 };
--- src/routes/order.js
+++ src/routes/order.js
@@ -1,10 +1,55 @@
 const { Router } = require("express");
-const { Order } = require('../models/Order')
+const { Order } = require("../models/Order");
+const {
+  getAllOrders,
+  getOrderById,
+  getOrderByUserId,
+  addOrder,
+} = require("../controllers/order");
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('order')
-})
+//GET ALL ORDERS
+router.get("/", async (req, res) => {
+  try {
+    const result = await getAllOrders();
+    res.send(await result);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+//GET ORDER BY IT'S ID
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await getOrderById(id);
+    res.send(await result);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+//GET ORDER BY USER ID
+router.get("/user/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await getOrderByUserId(id);
+    res.send(await result);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+//CREATE AN ORDER
+router.post("/", async (req, res) => {
+  const { product_id, unit_price, quantity, userId } = req.body;
+  try {
+    const result = await addOrder(product_id, unit_price, quantity, userId);
+    res.send(result);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 module.exports = router;
