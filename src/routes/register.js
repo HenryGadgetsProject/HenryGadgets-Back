e58619247adffeabc75dea59df15d4faed22328a
Leonardo Rosales <leonardorosales1485@gmail.com>
2021-05-16 13:08:07 -0300

    No requieran modulos que no existen, giles

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { signUp } = require("../controllers/authentication");
+// const { signUp } = require("../controllers/authentication");
 const { User } = require("../../db");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
@@ -12,15 +12,15 @@ router.post('/', (req, res) => {
     const { id, first_name, last_name, email, password, country, city, street, addressnumber, postcode, is_admin } = req.body;
     //Encriptamos la pass
     let passwordHash = bcrypt.hashSync(password, Number.parseInt(authConfig.rounds));
-    User.create({ 
-        id, first_name, last_name, email, password: passwordHash, country, city, street, addressnumber,postcode, is_admin 
+    User.create({
+        id, first_name, last_name, email, password: passwordHash, country, city, street, addressnumber,postcode, is_admin
     }).then(user => {
-        let token = jwt.sign({ user: user }, authConfig.secret, { 
-            expiresIn: authConfig.expires 
+        let token = jwt.sign({ user: user }, authConfig.secret, {
+            expiresIn: authConfig.expires
         });
-        res.json({ 
-            user, 
-            token 
+        res.json({
+            user,
+            token
         });
     }).catch(err => {
         res.status(500).json(err);
