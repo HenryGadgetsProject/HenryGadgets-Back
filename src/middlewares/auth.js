62fc99749e4ac20a338cdaa16b3385e65521809b
macamparone <marco43@hotmail.com>
2021-05-21 15:04:35 -0300

    FixAdminGuest

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,28 +1,39 @@
-const jwt = require('jsonwebtoken');
-const authConfig = require('../auth')
+const jwt = require("jsonwebtoken");
+const authConfig = require("../auth");
 
 const Authentication = async (req, res, next) => {
-    if(req.url === '/login' || req.url === '/register'){
-        next()
-    }else{
-        if(!req.headers.authorization) {
-            res.status(401).json({ msg: "Unauthorized access" })
+  if (req.url === "/login" || req.url === "/register" || req.method === "GET") {
+    if (req.headers.authorization) {
+      //Comprobar validez del token
+      let token = req.headers.authorization.split(" ")[1];
+
+      jwt.verify(token, authConfig.secret, (err, decoded) => {
+        if (err) {
+          res.status(500).json({ msg: "Token not valid", err });
         } else {
-            //Comprobar validez del token
-            let token = req.headers.authorization.split(' ')[1];
-    
-            jwt.verify(token, authConfig.secret, (err, decoded) => {
-                if(err) {
-                    res.status(500).json({ msg: "Token not valid", err })
-                } else {
-                    req.user = decoded
-                    next();
-                }
-            })
+          req.user = decoded;
+          next();
         }
+      });
+    } else {
+      next();
     }
-    //Comprobar que existe el token
- 
+  } else {
+    if (!req.headers.authorization) {
+      res.status(401).json({ msg: "Unauthorized access" });
+    } else {
+      let token = req.headers.authorization.split(" ")[1];
+
+      jwt.verify(token, authConfig.secret, (err, decoded) => {
+        if (err) {
+          res.status(500).json({ msg: "Token not valid", err });
+        } else {
+          req.user = decoded;
+          next();
+        }
+      });
+    }
+  }
 };
 
 module.exports = Authentication;
