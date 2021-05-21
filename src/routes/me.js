4b87add5727c544082d76ca3dbd73baded8e6f92
macamparone <marco43@hotmail.com>
2021-05-20 23:00:06 -0300

    RouteMe

--- src/routes/me.js
+++ src/routes/me.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const { User } = require("../../db");
 const jwt = require("jsonwebtoken");
-const authConfig = require('../auth');
+
 const { getUserById } = require('../controllers/user')
 
 const router = Router();
@@ -9,10 +9,11 @@ const router = Router();
 router.get('/', async (req, res, next) => {
     try {
         if (req.user) {
-            const { id } = req.user;
-            const result = await getUserById(id);
-            res.json(result);
-        } else res.sendStatus(401);
+            var { user: { id }} = req.user;
+            const result = await getUserById(id);            
+            var { id, first_name, last_name, email, is_admin } = result
+            res.json({id, first_name, last_name, email, is_admin}); // envio el obj sin el pass
+        } else res.send("entra aca");
       } catch (error) {
         next(error);
       }
