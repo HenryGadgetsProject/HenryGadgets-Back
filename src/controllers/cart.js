fdf132b688f9f3430d2f09f1644714152e6dab45
ecampili <eduardo.campili@gmail.com>
2021-05-22 20:45:58 -0300

    getCart

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -10,7 +10,7 @@ const createGuestCart = async (req, res) => {
             },
         });
 
-        for(let i = 0; i < req.body.length;i++){
+        for (let i = 0; i < req.body.length; i++) {
             let detailCreate = await OrderDetail.create({
                 productId: req.body[i].id,
                 quantity: req.body[i].quantity,
@@ -49,9 +49,9 @@ const addCart = async (req, res) => {
                 productId: id,
                 quantity: quantity,
                 unit_price: product.price,
-        });
-        await detailCreate.setOrder(order[0].id);
-        res.json(detailCreate);
+            });
+            await detailCreate.setOrder(order[0].id);
+            res.json(detailCreate);
         }
     } catch (error) {
         res.json(error);
@@ -63,50 +63,52 @@ const getAllCarts = async (req, res) => {
 
     try {
         const order = await Order.findOne({
-        where: {
-            userId: user_id,
-            state: "cart",
-        },
-    });
-
-    if (order) {
-        const ordersDetail = await OrderDetail.findAll({
             where: {
-            orderId: order.id,
+                userId: user_id,
+                state: "cart",
             },
+
         });
-        ordersDetail ? res.json(ordersDetail) : res.send("Card Empty");
-    } else {
-      res.json([]);
+
+        if (order) {
+            const ordersDetail = await OrderDetail.findAll({
+                where: {
+                    orderId: order.id,
+                },
+                include: [{ model: Product }]
+            });
+            ordersDetail ? res.json(ordersDetail) : res.send("Card Empty");
+        } else {
+            res.json([]);
+        }
+    } catch (error) {
+        console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
 }
 
 const deleteAll = async (req, res) => {
     const { user_id } = req.params;
 
-  try {
-    const order = await Order.findOne({
-      where: {
-        userId: user_id,
-        state: "cart",
-      },
-    });
-    const orderDetails = await OrderDetail.findAll({
-      where: {
-        order_id: order.id,
-      },
-    });
+    try {
+        const order = await Order.findOne({
+            where: {
+                userId: user_id,
+                state: "cart",
+            },
+        });
+        const orderDetails = await OrderDetail.findAll({
+            where: {
+                order_id: order.id,
+            },
+        });
 
-    for (let i = 0; i < orderDetails.length; i++) {
-      await orderDetails[i].destroy();
+        for (let i = 0; i < orderDetails.length; i++) {
+            await orderDetails[i].destroy();
+        }
+        res.send({ msg: "All items have been deleted" });
+    } catch (error) {
+        console.log(error);
     }
-    res.send({ msg: "All items have been deleted" });
-  } catch (error) {
-    console.log(error);
-  }
 }
 
 const deleteCart = async (req, res) => {
@@ -120,15 +122,15 @@ const deleteCart = async (req, res) => {
             },
         });
 
-    const orderDetail = await OrderDetail.findOne({
-        where: {
-            productId: id,
-            orderId: order.id,
-        },
-    });
-    orderDetail.destroy();
-    //res.json(orderDetail);
-    res.send({ msg: "Item succesfully deleted" })
+        const orderDetail = await OrderDetail.findOne({
+            where: {
+                productId: id,
+                orderId: order.id,
+            },
+        });
+        orderDetail.destroy();
+        //res.json(orderDetail);
+        res.send({ msg: "Item succesfully deleted" })
     } catch (error) {
         console.log(error);
     }
