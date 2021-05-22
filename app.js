145eaf7030343f752b456654143977e9f1dfdb21
macamparone <marco43@hotmail.com>
2021-05-22 17:44:36 -0300

    RouteMe

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 
 //eliminar cuando hagamos deploy
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', REQUESTS);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
