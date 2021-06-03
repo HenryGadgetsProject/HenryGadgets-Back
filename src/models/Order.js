381c83cc863323c535bf3bfac8955b455cdbfccd
Leonardo Rosales <leonardorosales1485@gmail.com>
2021-06-02 21:05:16 -0300

    tweak/ModificacionesParaDummyData

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,37 +1,45 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, Sequelize } = require("sequelize");
 const DT = DataTypes;
 
 module.exports = (sequelize) => {
-    sequelize.define('order', {
-        id: {
-            type: DT.INTEGER,
-            primaryKey: true,
-            allowNull: false,
-            autoIncrement: true
-        },
-        state: {
-            type: DT.ENUM({
-                values: ["cart", "created", "processing", "cancelled", "completed"],
-                allowNull: false,
-            }),
-        },
-        total_price: {
-            type: DT.FLOAT,
-        },
-        city: {
-            type: DT.STRING,
-        },
-        street: {
-            type: DT.STRING,
-        },
-        phone_number: {
-            type: DT.STRING,
-        },
-        country: {
-            type: DT.STRING,
-        },
+  sequelize.define(
+    "order",
+    {
+      id: {
+        type: DT.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      state: {
+        type: DT.ENUM({
+          values: ["cart", "created", "processing", "cancelled", "completed"],
+          allowNull: false,
+        }),
+      },
+      total_price: {
+        type: DT.FLOAT,
+      },
+      city: {
+        type: DT.STRING,
+      },
+      street: {
+        type: DT.STRING,
+      },
+      phone_number: {
+        type: DT.STRING,
+      },
+      country: {
+        type: DT.STRING,
+      },
+      created_at: {
+        type: DT.DATE,
+        defaultValue:  Sequelize.NOW
+      },
     },
-        {
-            underscored: true
-        });
-}
+    {
+      timestamps: false,
+      underscored: true,
+    }
+  );
+};
