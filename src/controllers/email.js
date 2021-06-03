735dc7aa92ca6e361d0d70ae6497e1def23e156d
jblici <juambif@gmail.com>
2021-06-03 15:50:05 -0300

    emails fixeados, stock, offers

--- src/controllers/email.js
+++ src/controllers/email.js
@@ -62,7 +62,8 @@ const emailBuyConfirmation = async (req, res) => {
       <h1>Hola ${client.first_name} ${client.last_name}!</h1>
       <p>Confirmación de pago ! ! !</p>
       </hr>
-      <b>Tu pedido: #${orderId.id}</b>
+      <b>Tu pedido: #${data.id}</b>
+      <b>Total: ${data.total_price}
       </hr>
       <p>Gracias por comprar en Henry Gadgets.</p>
       <p>Una vez que recibas tus productos podras dejar tu opionon sobre ellos.</p>
@@ -101,8 +102,7 @@ const emailBuyConfirmation = async (req, res) => {
 
 const emailThankYou = async (req, res) => {
   try {
-      const { products, client, orderId } = req.body
-      const data = await Order.findByPk(orderId)
+      const { products, client } = req.body
       let product
       if(!products) return res.status(500).json({ error: "product missing" })
       if(!Array.isArray(products)) return res.status(500).json({ error: "products should be and array" });
@@ -169,11 +169,6 @@ const emailThankYou = async (req, res) => {
       </hr>
       <p>Vamos a enviarte tu pedido una vez que confirmemos el pago, que puede demorar hasta 24hs.</p>
       <p>No te preocupes, te vamos a enviar un mensaje cuando esto suceda.</p>
-      <p>Datos de envio:<br>
-      ${client.first_name} ${client.last_name}<br>
-      ${data.street}<br>
-      ${data.country}, ${data.city}<br>
-      ${data.phone_number}</p><br>
       <b>Gracias por confiar en nosotros!</b>
       <div class="img-card">
       <img src="https://i.imgur.com/khqsGzc.png" width="200px" height="200px" alt="apus" border="0"/>
@@ -200,98 +195,98 @@ const emailThankYou = async (req, res) => {
     }
 }
 
-const passwordReset = async (req, res) => {
-  try {
-      const { email } = req.body
+// const passwordReset = async (req, res) => {
+//   try {
+//       const { email } = req.body
   
-      let transporter = nodemailer.createTransport({
-          service: "Gmail",
-          host: 'smtp.gmail.com',
-          post: 587,
-          secure: false,
-          auth: {
-              user: user,
-              pass: pass
-          },
-      });
+//       let transporter = nodemailer.createTransport({
+//           service: "Gmail",
+//           host: 'smtp.gmail.com',
+//           post: 587,
+//           secure: false,
+//           auth: {
+//               user: user,
+//               pass: pass
+//           },
+//       });
   
-      let htmlCreator = `
-      <html>
-      <head>
-      <style type="text/css">
-      .containergral {
-          align-content: center;
-          justify-content: center;
-          padding: 30px;
-          position: relative;
-          background: #EFEFEF;
-          }
-      h1 {
-          color: #FF1744;
-      }
-      .unorderlist {
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-          justify-content: center;
-          background: #F7F7F7;
-          color: #FF1744;
-        }
-      .img-card {
-          margin-left: 25%;
-          margin-top: 20px    
-      }
-      </style>
-      </head>
-      <body>
-      <div class="containergral">
-      <h1>Reset your password!</h1>
-      <p>Confirmación de orden ! ! !</p>
-      </hr>
-      <b>Tu lista de productos:</b>
-      <div class="unorderlist">
-        <ul>
-          ${products.map((e) => 
-            `<li>
-              <span>${e.quantity} x ${e.name} por ${e.price}</span>
-            </li>`)}
-        </ul>
-        <br>
-        <p>Total: ${data.total_price}</p>
-      </div>
-      </hr>
-      <p>Vamos a enviarte tu pedido una vez que confirmemos el pago, que puede demorar hasta 24hs.</p>
-      <p>No te preocupes, te vamos a enviar un mensaje cuando esto suceda.</p>
-      <p>Datos de envio:<br>
-      ${client.first_name} ${client.last_name}<br>
-      ${data.street}<br>
-      ${data.country}, ${data.city}<br>
-      ${data.phone_number}</p><br>
-      <b>Gracias por confiar en nosotros!</b>
-      <div class="img-card">
-      <img src="https://i.imgur.com/khqsGzc.png" width="200px" height="200px" alt="apus" border="0"/>
-      </div>
-      </div>
-      </body>
-      </html>
-      `;
+//       let htmlCreator = `
+//       <html>
+//       <head>
+//       <style type="text/css">
+//       .containergral {
+//           align-content: center;
+//           justify-content: center;
+//           padding: 30px;
+//           position: relative;
+//           background: #EFEFEF;
+//           }
+//       h1 {
+//           color: #FF1744;
+//       }
+//       .unorderlist {
+//           display: flex;
+//           flex-direction: row;
+//           align-items: center;
+//           justify-content: center;
+//           background: #F7F7F7;
+//           color: #FF1744;
+//         }
+//       .img-card {
+//           margin-left: 25%;
+//           margin-top: 20px    
+//       }
+//       </style>
+//       </head>
+//       <body>
+//       <div class="containergral">
+//       <h1>Reset your password!</h1>
+//       <p>Confirmación de orden ! ! !</p>
+//       </hr>
+//       <b>Tu lista de productos:</b>
+//       <div class="unorderlist">
+//         <ul>
+//           ${products.map((e) => 
+//             `<li>
+//               <span>${e.quantity} x ${e.name} por ${e.price}</span>
+//             </li>`)}
+//         </ul>
+//         <br>
+//         <p>Total: ${data.total_price}</p>
+//       </div>
+//       </hr>
+//       <p>Vamos a enviarte tu pedido una vez que confirmemos el pago, que puede demorar hasta 24hs.</p>
+//       <p>No te preocupes, te vamos a enviar un mensaje cuando esto suceda.</p>
+//       <p>Datos de envio:<br>
+//       ${client.first_name} ${client.last_name}<br>
+//       ${data.street}<br>
+//       ${data.country}, ${data.city}<br>
+//       ${data.phone_number}</p><br>
+//       <b>Gracias por confiar en nosotros!</b>
+//       <div class="img-card">
+//       <img src="https://i.imgur.com/khqsGzc.png" width="200px" height="200px" alt="apus" border="0"/>
+//       </div>
+//       </div>
+//       </body>
+//       </html>
+//       `;
   
-    let mailOptions = {
-      from: "Henry Gadgets <henrygadgetsofficial@gmail.com>",
-      to: client.email,
-      subject: `Has comprado ${product}`,
-      html: htmlCreator,
-    };
+//     let mailOptions = {
+//       from: "Henry Gadgets <henrygadgetsofficial@gmail.com>",
+//       to: client.email,
+//       subject: `Has comprado ${product}`,
+//       html: htmlCreator,
+//     };
   
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) return res.status(500).send(error.message);
+//     transporter.sendMail(mailOptions, (error, info) => {
+//       if (error) return res.status(500).send(error.message);
   
-      res.status(200).json({ answer: req.body });
-    });
-    } catch (error) {
-      res.send(error)
-    }
-}
+//       res.status(200).json({ answer: req.body });
+//     });
+//     } catch (error) {
+//       res.send(error)
+//     }
+// }
 
 const sendOffersNotification = async (req, res) => {
   let transporter = nodemailer.createTransport({
@@ -347,7 +342,7 @@ const sendOffersNotification = async (req, res) => {
               <div style="height: 100%; padding: 2rem; background-image: url('https://i.imgur.com/hRRqwnC.png'); background-size: 100%;">
                   <div style="border-radius: 2em;text-align: -webkit-center;background: rgba(0,0,0,.5);;z-index: 1;position: relative;color: #ffffff;padding: 1.6rem;">
                       <h1 font-size: 3rem;margin: 0;">Henry Gadgets!</h1>
-                      <p style="color: #FF1744; font-size: 1.5rem;">Hi ${user.first_name}!</p>
+                      <p style="color: #FF1744; font-size: 1.5rem;">Hola ${user.first_name}!</p>
                       <div style="margin: 1rem 0 1rem 0; background: #FF1744; padding: .3rem;border-radius: 20rem;">
                           <span style="font-size: 1.8rem;color: #ffffff;">Nuevos productos en oferta!</span>
                           <p style="font-size: 1.3rem;color: #ffffff;">Visita <a href="https://henrygadgets.vercel.app/">Henry Gadgets</a> para ver todas las ofertas...</p>
@@ -378,4 +373,103 @@ const sendOffersNotification = async (req, res) => {
   })
 }
 
-module.exports = { emailBuyConfirmation, passwordReset, emailThankYou, sendOffersNotification }
+const sendStockNotification = async (req, res) => {
+  const { id } = req.params
+  let transporter = nodemailer.createTransport({
+    service: "Gmail",
+    host: 'smtp.gmail.com',
+    post: 587,
+    secure: false,
+    auth: {
+        user: user,
+        pass: pass
+    },
+  });
+  const users = await User.findAll();
+  const product = await Product.findByPk(id);
+
+  users.forEach(async (user) => {
+    try {
+      let wishlists = await Wishlist.findOne({
+        where: {
+          user_id: userId
+        }, include: {
+          model: Product,
+          attributes: [
+            'id',
+            'name',
+            'price',
+            'description',
+            'big_image',
+            'rating',
+            'stock',
+          ]
+        }
+      })
+      const productFound = wishlists.products.filter(prod => prod.id === id)
+      if(productFound) {
+        if(user.nlsuscribe === true) {
+          let htmlCreator = `
+          <html>
+          <head>
+          <style type="text/css">
+          .containergral {
+            align-content: center;
+            justify-content: center;
+            padding: 30px;
+            position: relative;
+            background: #EFEFEF;
+          }
+          h1 {
+            color: #FF1744;
+          }
+          .unorderlist {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: center;
+            background: #F7F7F7;
+            color: #FF1744;
+          }
+          .img-card {
+            margin-left: 25%;
+            margin-top: 20px    
+          }
+          </style>
+          </head>
+          <body style="height: 100%; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';">
+              <div style="height: 100%; padding: 2rem; background-image: url('https://i.imgur.com/hRRqwnC.png'); background-size: 100%;">
+                  <div style="border-radius: 2em;text-align: -webkit-center;background: rgba(0,0,0,.5);;z-index: 1;position: relative;color: #ffffff;padding: 1.6rem;">
+                      <h1 font-size: 3rem;margin: 0;">Henry Gadgets!</h1>
+                      <p style="color: #FF1744; font-size: 1.5rem;">Hola ${user.first_name}!</p>
+                      <div style="margin: 1rem 0 1rem 0; background: #FF1744; padding: .3rem;border-radius: 20rem;">
+                          <span style="font-size: 1.8rem;color: #ffffff;">${product.name} en stock!</span>
+                          <p style="font-size: 1.3rem;color: #ffffff;">Visita <a href="https://henrygadgets.vercel.app/">Henry Gadgets</a>...</p>
+                      </div>
+                  </div>
+              </div>
+          </body>
+          </html>
+          `
+
+          let mailOptions = {
+            from: "Henry Gadgets <henrygadgetsofficial@gmail.com>",
+            to: user.email,
+            subject: `${user.first_name} ${product.name} esta en stock!!!`,
+            html: htmlCreator,
+          }; 
+
+          transporter.sendMail(mailOptions, (error, info) => {
+            if (error) return res.status(500).send(error.message);
+    
+            res.status(200).json({ answer: req.body });
+          });
+        }
+      }
+    } catch (error) {
+        console.log(error)
+    }
+  })
+}
+
+module.exports = { emailBuyConfirmation, emailThankYou, sendOffersNotification, sendStockNotification }
--- src/data/users.js
+++ src/data/users.js
@@ -8,7 +8,8 @@ const users = [
         "$2b$10$NvTinNrPRyFN3gojHaaD4OjhKdqi.IqovVh.p1L3YsrCdfu6o2TIO",
       reviews: [1, 2],
       products: [],
-      orderId: [2, 3, 17, 19]
+      orderId: [2, 3, 17, 19],
+      nlsuscribe: false,
     },
     {
       first_name: "Guille",
@@ -32,6 +33,7 @@ const users = [
       orderId: [4, 5, 18, 22, 23],
       reviews: [5, 6],
       products: ["9feeb2bb-3b51-4c15-9abc-4d1caf5860bc", "9feeb2bb-3b51-4c15-9abc-4d1caf5860cf", "9feeb2bb-3b51-4c15-9abc-4d1caf5860ca", "9feeb2bb-3b51-4c15-9abc-4d1caf5860bb", "9feeb2bb-3b51-4c15-9abc-4d1caf5860bb", "d7fe65a2-7aa4-4cbc-827e-f1a4e750d1f6", "2259c3df-8c19-44ca-ae73-31614d480ba9", "ea868843-211d-478b-a0c7-a8d6d97a371e", "ea868843-211d-478b-a0c7-a8d6d97a371f", "8b9c38e5-0a78-4a29-9773-98c264786f94"],
+      nlsuscribe: true,
     },
     {
       first_name: "Alex",
@@ -43,6 +45,7 @@ const users = [
       orderId: [15, 29, 30, 24, 25],
       products: ["70b9bcc3-3780-443b-8182-ef56e5468700", "70b9bcc3-3780-443b-8182-ef56e5468703", "70b9bcc3-3780-443b-8182-ef56e5468903", "70b9bcc3-3780-443b-8182-ef56e5468733", "70b9bcc3-3780-443b-8182-ef56e5468743", "70b9bcc3-3780-443b-8182-ef56e5468150", "70b9bcc3-3780-443b-8182-ef56e5468560", "70b9bcc3-3780-443b-8182-ef56e5468780", "70b9bcc3-3780-443b-8182-ef56e5468990", "70b9bcc3-3780-443b-8182-ef56e5468000"],
       reviews: [7, 8],
+      nlsuscribe: false,
     },
     {
       first_name: "Enzo", 
@@ -54,6 +57,7 @@ const users = [
       orderId: [6, 7, 8, 26, 27],
       products: [],
       reviews: [9, 10],
+      nlsuscribe: false,
     },
     {
       first_name: "Juan",
@@ -77,6 +81,7 @@ const users = [
       orderId: [13, 14, 28],
       products: [],
       reviews: [13, 14, 15],
+      nlsuscribe: false,
     },
   ]
 
